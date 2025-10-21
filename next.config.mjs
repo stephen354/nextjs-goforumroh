0351a0eaeac3e76865de7265adef380397648548
Stephen Malik <stephenjokam354@gmail.com>
2025-10-21 08:04:08 +0700

    finish-error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
