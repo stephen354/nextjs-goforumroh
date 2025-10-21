085ce26f391037e48b0b8be10a46b55d408c0ff5
Stephen Malik <stephenjokam354@gmail.com>
2025-10-21 08:00:17 +0700

    finish

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+'use client'
 import Header from "@/components/Header";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  
+  const router = useRouter()
+  router.push("/login");
   return (
     <Header/>
   );
