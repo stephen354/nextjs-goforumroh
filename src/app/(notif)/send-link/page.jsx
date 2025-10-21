af4b1867c9018f56cc0a74ce1b7c5c45fbdd4f91
Stephen Malik <stephenjokam354@gmail.com>
2025-10-21 08:23:49 +0700

    finish-error-client

--- src/app/(notif)/send-link/page.jsx
+++ src/app/(notif)/send-link/page.jsx
@@ -1,29 +1,8 @@
-"use client";
 
-import {Button, ButtonLight} from "@/components/forms/Button";
-import AuthTitle from "@/components/forms/AuthTitle";
-import { useSearchParams } from "next/navigation";
-import "@/styles/layout/_auth-verify.scss";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import SendLink from "./SendLink";
 
-export const dynamic = 'force-dynamic';
-
-export default function SendLink(){
-   const searchParams = useSearchParams();
-   const email = searchParams.get("email");
-   const router = useRouter() ;
-
-   const handleNext = () =>{
-        router.push(`https://mail.google.com/mail/u/0`);
-   }
-    return (
-        <div className="verify-wrapper">
-            <div className="verify-content">
-                <Image src={"/icons/checklist.png"} alt="checklist" width={72} height={72}/>
-                 <AuthTitle className="title" title={"Check your inbox"} subtitle={<>We just emailed instructions and a reset password link to <strong>{email}</strong>. It might take a few minutes to arrive.</>}/>
-                <Button onClick={handleNext}>Continue</Button>
-            </div>
-            </div>
-    )
+export default function Page() {
+  return (
+      <SendLink />
+  );
 }
--- src/app/(notif)/verivy/page.jsx
+++ src/app/(notif)/verivy/page.jsx
@@ -1,29 +1,8 @@
-"use client";
 
-import {Button, ButtonLight} from "@/components/forms/Button";
-import AuthTitle from "@/components/forms/AuthTitle";
-import { useSearchParams } from "next/navigation";
-import "@/styles/layout/_auth-verify.scss";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Verify from "./VerivyLink";
 
-export const dynamic = 'force-dynamic';
-
-export default function verivy(){
-   const searchParams = useSearchParams();
-   const email = searchParams.get("email");
-   const router = useRouter() ;
-
-   const handleNext = () =>{
-        router.push(`https://mail.google.com/mail/u/0`);
-   }
-    return (
-        <div className="verify-wrapper">
-            <div className="verify-content">
-                <Image src={"/icons/checklist.png"} alt="checklist" width={72} height={72}/>
-                 <AuthTitle className="title" title={"Verify your email address"} subtitle={<>We sent you an email with a verification link to <strong>{email}</strong>  To confirm your account please follow the link in the email we just sent.</>}/>
-                <Button onClick={handleNext}>Continue</Button>
-            </div>
-            </div>
-    )
+export default function Page() {
+  return (
+      <Verify />
+  );
 }
