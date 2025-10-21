2d3542bf8ee983d13b74e9c0071285530c5369df
Stephen Malik <stephenjokam354@gmail.com>
2025-10-21 08:18:04 +0700

    finish-error

--- src/app/(notif)/verivy/page.jsx
+++ src/app/(notif)/verivy/page.jsx
@@ -7,6 +7,7 @@ import "@/styles/layout/_auth-verify.scss";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
 
 export default function verivy(){
    const searchParams = useSearchParams();
